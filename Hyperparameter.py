25548dc3f8cb4810bb4d822ba59f4fde6259b0da
Krish C Naik <krishnaik06@gmail.com>
2019-08-05 21:51:31 +0530

    Add files via upload

---
+++ Hyperparameter.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 13:30:38 2019
+
+@author: krish.naik
+"""
+
+# Artificial Neural Network
+
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13]
+y = dataset.iloc[:, 13]
+
+#Create dummy variables
+geography=pd.get_dummies(X["Geography"],drop_first=True)
+gender=pd.get_dummies(X['Gender'],drop_first=True)
+
+## Concatenate the Data Frames
+
+X=pd.concat([X,geography,gender],axis=1)
+
+## Drop Unnecessary columns
+X=X.drop(['Geography','Gender'],axis=1)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+## Perform Hyperparameter Optimization
+
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Embedding, Flatten, LeakyReLU, BatchNormalization, Dropout
+from keras.activations import relu, sigmoid
+
+
+
+def create_model(layers, activation):
+    model = Sequential()
+    for i, nodes in enumerate(layers):
+        if i==0:
+            model.add(Dense(nodes,input_dim=X_train.shape[1]))
+            model.add(Activation(activation))
+            model.add(Dropout(0.3))
+        else:
+            model.add(Dense(nodes))
+            model.add(Activation(activation))
+            model.add(Dropout(0.3))
+            
+    model.add(Dense(units = 1, kernel_initializer= 'glorot_uniform', activation = 'sigmoid')) # Note: no activation beyond this point
+    
+    model.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+    return model
+    
+model = KerasClassifier(build_fn=create_model, verbose=0)
+
+
+layers = [[20], [40, 20], [45, 30, 15]]
+activations = ['sigmoid', 'relu']
+param_grid = dict(layers=layers, activation=activations, batch_size = [128, 256], epochs=[30])
+grid = GridSearchCV(estimator=model, param_grid=param_grid,cv=5)
+
+grid_result = grid.fit(X_train, y_train)
+
+[grid_result.best_score_,grid_result.best_params_]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
